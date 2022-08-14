04113c0e2464ed7cb6579580d69777844566b11b
Manuel Kasper <mk@neon1.net>
2022-08-14 14:11:31 +0200

    Changes for running under Linux

--- update.sh
+++ update.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-cd /data/tiles
+cd /app/sotlas-tiles
 rm -f summitslist.csv
-/usr/local/bin/wget -q https://www.sotadata.org.uk/summitslist.csv
-/usr/local/bin/php makegeojson.php
-/usr/local/bin/tippecanoe -qQ -z10 -B3 -pi -pg --force --drop-densest-as-needed --extend-zooms-if-still-dropping -o summits.mbtiles summits.geojson
-/usr/local/bin/tippecanoe -qQ -z10 -B3 -pi -pg --force --drop-densest-as-needed --extend-zooms-if-still-dropping -o summits_inactive.mbtiles summits_inactive.geojson
-/usr/local/bin/tippecanoe -qQ -z10 -B3 -pi -pg --force --drop-densest-as-needed --extend-zooms-if-still-dropping -o regions.mbtiles -L areas:regions_areas.geojson -L labels:regions_labels.geojson
-cp *.mbtiles /data/openmaptiles
-su pm2 -c "/usr/local/bin/pm2 restart tileserver" > /dev/null
+wget -q https://www.sotadata.org.uk/summitslist.csv
+php makegeojson.php
+tippecanoe -qQ -z10 -B3 -pi -pg --force --drop-densest-as-needed --extend-zooms-if-still-dropping -o summits.mbtiles summits.geojson
+tippecanoe -qQ -z10 -B3 -pi -pg --force --drop-densest-as-needed --extend-zooms-if-still-dropping -o summits_inactive.mbtiles summits_inactive.geojson
+tippecanoe -qQ -z10 -B3 -pi -pg --force --drop-densest-as-needed --extend-zooms-if-still-dropping -o regions.mbtiles -L areas:regions_areas.geojson -L labels:regions_labels.geojson
+cp *.mbtiles /data/tiles
+su pm2 -c "pm2 restart tileserver" > /dev/null
 
 # Copy tiles to US map server and restart tile server
-scp -q *.mbtiles root@map-us.sotl.as:/data/openmaptiles
-ssh root@map-us.sotl.as 'su pm2 -c "/usr/local/bin/pm2 restart tileserver" > /dev/null'
+scp -q *.mbtiles root@map-us.sotl.as:/data/tiles
+ssh root@map-us.sotl.as 'su pm2 -c "pm2 restart tileserver" > /dev/null'
