9e238a29ec708ceb43926e11a54c92d1c7efb37e
Manuel Kasper <mk@neon1.net>
2026-07-30 20:38:47 +0200

    Add AZ polygons for W0C and W4V (contributed by N0QLR)

--- az/geojson2gpx/geojson2gpx.js
+++ az/geojson2gpx/geojson2gpx.js
@@ -2,13 +2,14 @@ const togpx = require('togpx')
 const reproject = require('reproject')
 const epsg = require('epsg')
 const fs = require('fs')
+const glob = require('glob')
 
-let inputfiles = fs.globSync('*/*.geojson')
+let inputfiles = glob.globSync('*/*/*.geojson')
 
 for (let inputfile of inputfiles) {
 	console.log(inputfile)
 	let geojson = JSON.parse(fs.readFileSync(inputfile, 'utf8'))
-	geojson = reproject.toWgs84(geojson, undefined, epsg)
+	//geojson = reproject.toWgs84(geojson, undefined, epsg)
 	let gpx = togpx(geojson)
 	outputfile = inputfile.replace('.geojson', '.gpx')
 	fs.writeFileSync(outputfile, gpx)
